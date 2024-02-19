26ab7eaab2b39031c0dbda3cca3e94f9267b6b96
eze-quielph <ezequiel.fotografiaa@gmail.com>
2024-02-18 23:11:53 -0600

    arreglo de posicionamiento de la home, agrego efecto de tipado en home y inicio configuracion de traduccion es/en

---
+++ src/i18n/ui.ts
@@ -0,0 +1,61 @@
+export const languages = {
+  en: "English",
+  es: "Spanish",
+};
+
+export const defaultLang = "en";
+
+export const ui = {
+  en: {
+    // Layout
+    "layout.title": "Ezequiel Benitez | Freelance, Software Engineer",
+    "layout.description":
+      "Ezequiel Benitez | Freelance Software Engineer experience +3 years Backend Developer",
+
+    // Header
+    "header.home": "Home",
+    "header.experience": "Experience",
+    "header.project": "Projects",
+    "header.about": "About",
+    "header.contact": "Contact",
+
+    // Home
+    "home.title": "Hello! 👋 I'm Ezequiel Benitez",
+    "home.badge": "Open to Opportunities",
+    "home.description.span-1":
+      "Over 20 satisfied clients and 2 years of experience.",
+    "home.description.span-2":
+      "You can visit my blog where I talk about programming and technical topics.",
+    "home.description.span-3": "I'm from Buenos Aires, Argentina.",
+    "home.description.span-4": "Specialized in Backend Development.",
+    "home.input-1": "Software Engineer ",
+    "home.input-2": "Mobile Developer ",
+    "home.input-3": "Web Developer ",
+  },
+  es: {
+    // Layout
+    "layout.title": "Ezequiel Benitez | Freelance, Ingeniero de Software",
+    "layout.description":
+      "Ezequiel Benitez | Freelance, Ingeniero de Software, más de 3 años de experiencia como desarrollador Backend",
+   
+      // Header
+    "header.home": "Inicio",
+    "header.experience": "Experiencia",
+    "header.project": "Proyectos",
+    "header.about": "Sobre Mi",
+    "header.contact": "Contacto",
+
+    // Home
+    "home.title": "¡Hola! 👋 Soy Ezequiel Benitez",
+    "home.badge": "Abierto a Oportunidades",
+    "home.description.span-1":
+      "Más de 20 clientes satisfechos y 2 años de experiencia.",
+    "home.description.span-2":
+      "Puedes visitar mi blog donde hablo de programación y temas técnicos.",
+    "home.description.span-3": "Soy de Buenos Aires, Argentina.",
+    "home.description.span-4": "Especializado en Desarrollo Backend.",
+    "home.input-1": "Software Engineer  ",
+    "home.input-2": "Mobile Developer  ",
+    "home.input-3": "Web Developer  ",
+  },
+} as const;
